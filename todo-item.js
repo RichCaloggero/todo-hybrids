9a216ffc31ceee107baed9533437dc900927fea8
Rich <rjc@mit.edu>
2020-09-28 10:24:49 -0400

    * it now does the following:
    - adds child <todo-item> elements to the list on connect()
       + any added later via appendChild() etc will be ignored
    - maintains separate lists per <todo-list> element (not yet tested)
    - editing the current todo via keyboard
    - focus management seems to work well
       + may be some untested scenareos
    - escape key cancels edit dialog
    - must edit to change state
       + consider requiring dialog only to change item content rather than state

--- todo-item.js
+++ todo-item.js
@@ -1,87 +1,11 @@
 import {parent, define, html, property} from "./hybrids/index.js";
-import * as todoList from "./todo-list.js";
 
-const TodoListItem = {
-todoList: parent(todoList.TodoList),
-index: 0,
-
-edit: false,
-
-editButton: todoList.ref("#edit"),
-completeInput: todoList.ref("#complete-label input"),
-
-complete: {
-	connect: (host, key) => host[key] = false,
-	observe: (host, value) => host.edit && host.completeInput.focus()
-}, // complete
-
-title: "",
-body: {
-get: (host, value) => host.textContent,
-set: (host, value) => host.textContent = value
-}, // body
-
-
-render: ({ focus, edit, index, complete, title, body }) => {
-console.debug("rendering item: ", index, title, body, complete);
-return html`
-<div class="item" role="document">
-<div class="header">
-<h3><button id="edit" aria-pressed="${edit? 'true' : 'false'}" onclick="${_edit}">
-${title}
-<span>(${complete? "completed" : "in progress"})</span>
-</button></h3>
-<button onclick="${_remove}" aria-label="remove ${title}">Remove</button>
-</div>
-
-${edit? renderForm() : html`<div class="body">${body}</div>`}
-</div><!-- todo item -->
-`;
-
-function _edit (host) {
-	host.edit = true;
-//host.focus = "#complete-label input";
-host.completeInput.focus();
-} // _edit
-
-function renderForm () {
-return html`
-<div role="dialog" aria-label="Edit">
-<label id="complete-label">Complete: ${complete?
-html`<input type="checkbox"  checked onchange="${html.set("complete")}">`
-: html`<input type="checkbox"  onchange="${html.set("complete")}">`
-}</label>
-
-<label>Title: <input type="text" defaultValue="${title}" onchange="${html.set("title")}"></label>
-
-<label>Body: <br><textarea onchange="${html.set("body")}">${body}</textarea></label>
-
-<div class="actions">
-<button onclick="${_save}">Save</button>
-<button onclick="${_close}">Cancel</button>
-</div>
-</div>
-`; // html
-} // renderForm
-
-
-function _close (host, event) {
-	host.edit = false;
-host.editButton.focus();
-} // _close
-
-function _save (host) {
-todoList.update(host.index, {complete: host.complete, title: host.title, body: host.body}, host.todoList);
-_close(host);
-} // _save
-
-function _remove (host) {
-todoList.removeItem(host.index, host.todoList);
-_close(host);
-} // _remove
-
-} // render
-
-};
-
-define("todo-item", TodoListItem);
+export const TodoItem = {
+	complete: false,
+	title: "",
+	body: {
+		get: (host, value) => host.textContent,
+		set: (host, value) => host.textContent = value
+	}, // body
+}; // descriptors
+define("todo-item", TodoItem);
--- todo-list.js
+++ todo-list.js
@@ -1,4 +1,5 @@
-import {define, html, render} from "./hybrids/index.js";
+import {children, define, html, render} from "./hybrids/index.js";
+import {TodoItem} from "./todo-item.js";
 
 const todoItemTemplate = {
 complete: false,
@@ -6,30 +7,32 @@ title: "[new item]",
 body: ""
 };
 
-export const TodoList = {
-todoList: [],
-count: 0,
+const TodoList = {
+_children: children(TodoItem),
+
+todoList: {
+connect: (host, key) => host[key] = host._children.map(getItemProperties),
+}, // todoList
+
+edit: false,
+	
+complete: {
+	connect: (host, key) => host[key] = false,
+	observe: (host, value) => host.edit && host.completeInput.focus()
+}, // complete
+
+editButton: ref("#edit"),
 newButton: ref("#new"),
+completeInput: ref("#complete-label input"),
 
-
-render: ({ todoList}) => {
-console.debug("rendering todo list...");
+render: ({ todoList, edit }) => {
 return html`
 <fieldset><legend><h2>Todo Items</h2></legend>
 <div role="status" aria-atomic="true" id="status">${todoList.length} items.</div>
 <div><button id="new" onclick="${_createItem}">New</button></div>
-<hr><ul>
-${todoList.map((data, index) => {
-const {complete, title, body} = {...data};
-console.debug("render list: ", complete, title, body);
-return html`
-<li><todo-item index="${index}" complete="${complete? 'complete' : ''}" title="${title}">
-${body}
-</todo-item></li>
-`;
-}) // map
-}</ul>
-</fieldset>
+<hr><ul style="list-style-type: none;">
+${todoList.map(renderTodoItem)}
+</ul></fieldset>
 `;
 
 function _createItem (host) {
@@ -37,29 +40,105 @@ function _createItem (host) {
 addItem(host, todoItemTemplate);
 } // _createItem
 
-} // render
-};
+function renderTodoItem ({complete, title, body}, index) {
+return html`<li><div class="item" role="document">
+<div class="header">
+<h3><button id="edit" aria-pressed="${edit? 'true' : 'false'}" onclick="${_edit}">
+${title}
+<span>(${complete? "completed" : "in progress"})</span>
+</button></h3>
+<button onclick="${_remove}" aria-label="remove ${title}">Remove</button>
+</div>
+
+${edit? renderForm({index, complete, title, body}) : html`<div class="body">${body}</div>`}
+</div></li>
+`;
+
+function _edit (host) {
+	host.edit = true;
+host.completeInput.focus();
+console.debug("edit mode: ", host.edit);
+} // _edit
+
+function _remove (host) {
+removeItem(index, host);
+_close(host);
+} // _remove
+} // renderTodoItem
+
+function renderForm ({index, complete, title, body}) {
+const data = {complete, title, body};
+console.debug("renderForm: ", index, complete, title, body);
+return html`
+<div role="dialog" aria-label="Edit" onkeydown="${_handleShortcutKey}">
+<label id="complete-label">Complete:
+${complete?
+html`<input type="checkbox" checked onchange="${(host, event) => _set("complete", false)}">`
+: html`<input type="checkbox" onchange="${(host, event) => _set("complete", true)}">`
+}
+</label>
+
+<label>Title: <input type="text" defaultValue="${title}" onchange="${(host, event) => _set("title", event.target.value)}"></label>
+
+<label>Body: <br><textarea onchange="${(host, event) => _set("body", event.target.value)}">${body}</textarea></label>
+
+<div class="actions">
+<button onclick="${_save}">Save</button>
+<button onclick="${_close}">Cancel</button>
+</div>
+</div>
+`; // html
+
+function _handleShortcutKey (host, event) {
+	if (event.key === "Escape") {
+		event.preventDefault();
+_close(host);
+	/*} else if (event.altKey && event.shiftKey && event.key === "s") {
+				event.preventDefault();
+_save(host);
+	*/
+	} // if
+	} // _handleShortcutKey
+
+function _set (prop, value) {
+	data[prop] = value;
+console.debug(`setting ${prop} to ${value}`, data);
+} // _set
+
+function _close (host, event) {
+	host.edit = false;
+host.editButton.focus();
+} // _close
+
+function _save (host) {
+console.debug("save: ", index, complete, title, body);
+update(index, {...data}, host);
+_close(host);
+} // _save
+} // renderForm
+
+}, // render
+}; // descriptors
 
 define("todo-list", TodoList);
 
 export function addItem (host, data) {
 if (host.todoList.find(_data => _data.title === data.title)) return;
-host.todoList = host.todoList.concat({...data});
+host.todoList = [{...data}].concat(host.todoList);
 } // addItem
 
 
-export function update (index, data, parent) {
+export function update (index, data, host) {
 console.debug("updating ", data);
-parent.todoList = [].concat(
+host.todoList = [].concat(
 data,
-parent.todoList.filter((x, i) => i !== index)
+host.todoList.filter((x, i) => i !== index)
 ); // concat
 } // update
 
-export function removeItem  (index, parent) {
-console.debug("removing: ", parent.tagName, index);
-parent.todoList = parent.todoList.filter((x, i) => i !== index);
-parent.newButton.focus();
+export function removeItem  (index, host) {
+host.todoList = host.todoList.filter((x, i) => i !== index);
+host.newButton.focus();
 } // removeItem
 
 export function ref(query) {
@@ -70,5 +149,13 @@ return target.querySelector(query);
 } // if
 
 return null;
-}; // return
+}; // return function
 } // ref
+
+function getItemProperties (item) {
+const props =  Object.fromEntries(
+Object.keys(todoItemTemplate).map(key => [key, item[key]])
+); // fromEntries
+console.debug("item properties: ", props);
+return props;
+} // getItemProperties
